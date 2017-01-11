5b80c6fa0bd8191d426d5eff801a0d4f4efeebf9
student-01 <student-01>
2017-01-11 15:50:05 -0800

    Jan11-Intensify

---
+++ jan09-intensify/js/script.js
@@ -0,0 +1,9 @@
+var sn_open = document.getElementById("sn-open"),
+ sn = document.getElementById("sn"),
+ sn_close = document.getElementById("sn-close");
+
+sn_open.addEventListener("click" , runAlert);
+
+function runAlert(){
+  alert(" can you smell what the rock is cooking? ");
+}
