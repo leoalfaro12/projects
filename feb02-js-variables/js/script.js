c960db447c11782bc02f74e04dd8863486e883c0
student-01 <student-01>
2017-02-02 15:45:59 -0800

    JavaScript

---
+++ feb02-js-variables/js/script.js
@@ -0,0 +1,34 @@
+/*
+multiline comment
+var externalJavascript = "wat"
+*/
+ //single line comment
+
+var camelCase; //best choice
+var not_camel_case;
+var camel_Case;
+var $dollarCase;
+var dollar$case;
+var _underScore;
+var firstVar;
+var variable;
+
+//no. bad.
+var UppercaseCamel;
+var SUPERUPPERCASE;
+
+//javascript objects
+/*
+number
+string
+boolean
+float
+array
+object
+*/
+var myNumberObject = 3;
+
+var myNumberObject = myNumberObject.toString();
+
+console.log(myNumberObject);
+console.log(myNewNumberObject);
