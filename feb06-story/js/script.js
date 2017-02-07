8b8995144ec0da080eba2b2b573975f137c664a5
student-01 <student-01>
2017-02-07 13:29:39 -0800

    So Far

---
+++ feb06-story/js/script.js
@@ -0,0 +1,42 @@
+var firstName = prompt('What is your name?');
+var intro = 'Where are you from? ' + firstName + '.';
+
+console.log(firstName);
+alert(intro);
+
+var age = prompt('How old are you?'),
+
+
+alert(response);
+console.log(response);
+
+var first = confirm();
+console.log(first);
+
+alert();
+
+var second = confirm();
+
+alert();
+
+var third = prompt();
+console.log(third);
+alert();
+
+var fourth = confirm();
+console.log(fourth);
+
+var fifth = confirm();
+console.log(fifth);
+
+var sixth = prompt();
+console.log(sixth);
+
+if (!isNaN(sixth)){
+  alert();
+}else {
+  alert();
+}
+
+var seventh = confirm();
+console.log(seventh);
