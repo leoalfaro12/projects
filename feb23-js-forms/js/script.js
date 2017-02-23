26f2bc5b47ffd50c31718e3ba87d9c04f7b004be
student-01 <student-01>
2017-02-23 15:55:43 -0800

    forms

---
+++ feb23-js-forms/js/script.js
@@ -0,0 +1,12 @@
+var a = document.getElementById();
+var b = document.getElementByClassName();
+var c = document.querySelector();
+var d = document.querySelectorAll();
+var e = document.getElementByTagName();
+
+var form = document.querySelector('form')
+var subBtn = document.querySelector('input[type="submit"]')
+
+subBtn.addEventListener('click', function(){
+
+});
