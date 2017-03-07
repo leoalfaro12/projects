2c58e832b92a3fd7d092b586b1a5b4fb288301b2
student-01 <student-01>
2017-03-07 15:56:32 -0800

    modals #2

---
+++ mar07-js-modals/js/script.js
@@ -0,0 +1,71 @@
+// var content = document.getElementById('content');
+//
+// var h1 = document.createElement('h1'),
+//      p = document.createElement('p'),
+//      text = document.createTextMode('creating a text node')
+//      a = document.createElement('a');
+//
+// h1.textContent = 'text';
+// p.textContent = 'well hello there';
+// a.textContent = 'click';
+//
+// content.append(h1, p, text, 'text node', a);
+
+// // content.appendChild(h1);
+// function createModal(modalType modalMsg){
+// var modalForm = document.createElement('form'),
+//     modalMessage = document.createElement('p')
+//     modalInput = document.createElement('input'),
+//     modalConfirm = document.createElement('input'),
+//     modalCancel = document.createElement('input');
+//
+//   modalConfirm.type = 'submit';
+//   modalConfirm.value = 'Confirm';
+//
+//   modalCancel.type = 'button';
+//   modalCancel.value = 'cancel';
+//
+//   modalForm.append(modalInput);
+//   modalForm.append(modalCancel);
+//   modalForm.append(modalConfirm);
+//
+//   modal.append(modalMessage);
+//   if(modalType === 'prompt'){
+//      modal.append(modalInput);
+//      modal.append(modalCancel);
+//
+// modal.append(modalForm);
+//
+//
+//
+// });
+// }
+//
+// createModal('prompt');
+
+var form = document.querySelector('form');
+
+// console.log('Notifiation' in window);
+//
+// if( ('Notifiation' in window) ){
+//   alert('Nope, sorry');
+// }else{
+//   console.log('Not available');
+
+// form.forEach(function(e, i){
+//   console.log(e);
+// });
+
+// for(var i = 0; i < from.children.length; i++){
+//   console.log(form.children[i]);
+// }
+// console.log('childElementCount' in form);
+//
+// if(!'childElementCount' in form){
+//   for(var i = 0;)
+// }
+console.log(form);
+
+var formArr = [];
+
+console.log(formArr);
